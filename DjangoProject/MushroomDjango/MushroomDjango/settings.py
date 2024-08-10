89c26def02406f24c9bffdcd0228ad902a8a9a6b
SunnyFarmDay <yypoonaa@connect.ust.hk>
2024-08-11 07:53:22 +0800

    make allowed hosts to add more than 1

--- DjangoProject/MushroomDjango/MushroomDjango/settings.py
+++ DjangoProject/MushroomDjango/MushroomDjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "django-insecure-775k&^1=e#)*vd^^8bpds^9wx3
 #DEBUG = True
 DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST', 'localhost')]
+ALLOWED_HOSTS = [x.strip() for x in os.environ.get('ALLOWED_HOST', 'localhost').split(",")]
 
 
 # Application definition
